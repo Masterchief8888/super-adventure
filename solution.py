f5ca55a4dc552e35d505776e4e18021ef9acf902
KANDI_ABHISHEK <90146929+Masterchief8888@users.noreply.github.com>
2021-09-27 21:07:21 +0530

    solution.py
    #create a module
    def find_missing_letter(chars):
        missingChar = ''
    #loop
        for i in range(0,len(chars)-1):
            if(ord(chars[i+1]) - ord(chars[i]) > 1):
                missingChar = chr(ord(chars[i])+1)
    
        return missingChar

---
+++ solution.py
@@ -0,0 +1,9 @@
+#deine a module
+def find_missing_char(chars):
+    missingChar = ''
+    #loop
+    for i in range(0,len(chars)-1):
+        if(ord(chars[i+1]) - ord(chars[i]) > 1):
+            missingChar = chr(ord(chars[i])+1)
+
+    return missingChar
